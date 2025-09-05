3dcf7062e9fbf481240bc3019eb1cdce9bb1a2ce
Hal <grzegorz.gogolewski@gmail.com>
2025-09-05 09:14:33 +0200

    Poprawki

--- publish.bat
+++ publish.bat
@@ -2,11 +2,14 @@
 rem Ustawia bie¿¹cy katalog na folder, w którym znajduje siê skrypt
 cd /d "%~dp0"
 
-echo Publikowanie PDF_Inspektor i PDF_Inspektor_Updater...
+echo Publikowanie PDF_Inspektor (razem z Updaterem)...
 
-rem Publikuje ca³¹ solucjê dla architektury x64
-dotnet publish PDF_Inspektor.sln -c Release -r win-x64 --self-contained false /p:PublishSingleFile=false /p:PublishSingleFileUpdater=true
+rem Publikuje g³ówny projekt. Updater zostanie opublikowany automatycznie dziêki referencji.
+rem Wszystkie ustawienia (x64, SelfContained, PublishSingleFile) s¹ wczytywane z plików .csproj.
+dotnet publish PDF_Inspektor.sln -c Release
 
 echo.
-echo Publikowanie zakoñczone.
+echo Publikowanie zakoñczone. Pliki znajduj¹ siê w folderze:
+echo PDF_Inspektor\bin\Release\net8.0-windows\win-x64\publish\
+echo.
 pause
